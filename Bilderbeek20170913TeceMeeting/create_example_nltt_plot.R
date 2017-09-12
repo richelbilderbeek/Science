10cf49426be074f09602c5df8efac67e112f48f6
richelbilderbeek <richel@richelbilderbeek.nl>
2017-09-12 15:23:50 +0200

    EOD

--- Bilderbeek20170913TeceMeeting/create_example_nltt_plot.R
+++ Bilderbeek20170913TeceMeeting/create_example_nltt_plot.R
@@ -1,22 +1,21 @@
 library(nLTT) #nolint
 set.seed(42)
-#tree1 <- TESS::tess.sim.age(n = 1, age = 4, lambda = 0.4, mu = 0)[[1]]
-#tree2 <- TESS::tess.sim.age(n = 1, age = 4, lambda = 0.25, mu = 0)[[1]]
-
 tree1 <- ape::rcoal(n = 5)
 tree2 <- ape::rcoal(n = 5)
 
 svg("~/example_tree_1.svg")
-ape::plot.phylo(tree1, show.tip.label = FALSE)
+ape::plot.phylo(tree1, show.tip.label = FALSE, edge.width = 10, edge.color = "red")
 dev.off()
 
 svg("~/example_tree_2.svg")
-ape::plot.phylo(tree2, show.tip.label = FALSE)
+ape::plot.phylo(tree2, show.tip.label = FALSE, edge.width = 10, edge.color  = "blue")
 dev.off()
 
 svg("~/example_nltt_plot.svg")
-nltt_plot(tree1, col = "red")
-nltt_lines(tree2, col = "blue")
-legend("topleft", c("tree1", "tree2"), col = c("red", "blue"), lty = 1)
+ape::ltt.plot
+my_scale <- 2
+nLTT::nltt_plot(tree1, col = "red", lwd = 3, cex.axis = my_scale, cex.lab = my_scale)
+nltt_lines(tree2, col = "blue", lwd = 3)
+#legend("topleft", c("tree1", "tree2"), col = c("red", "blue"), lty = 1, lwd = 3)
 dev.off()
 
